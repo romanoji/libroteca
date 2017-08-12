85e9c3818015006070dcef77fb201c72e88fa0f5
Roman Józwiak <romanoji@users.noreply.github.com>
2017-08-12 04:47:59 +0200

    Add create & update book copies endpoints

--- src/UI/Web/Symfony/Controller/BookCopyController.php
+++ src/UI/Web/Symfony/Controller/BookCopyController.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 namespace RJozwiak\Libroteca\UI\Web\Symfony\Controller;
 
 use Ramsey\Uuid\Uuid;
+use RJozwiak\Libroteca\Application\Command\RegisterBookCopy;
+use RJozwiak\Libroteca\Application\Command\UpdateBookCopyRemarks;
 use RJozwiak\Libroteca\Application\Query\BookCopyQueryService;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/books/{bookID}/copies")
+ */
 class BookCopyController extends ApiController
 {
     /**
-     * @Route("/books/{bookID}/copies")
+     * @Route(methods={"GET"})
      */
     public function getByBookAction(string $bookID)
     {
@@ -18,6 +24,42 @@ class BookCopyController extends ApiController
             return $this->successResponse(
                 $this->booksCopies()->getAllByBook(Uuid::fromString($bookID)->toString())
             );
+        });
+    }
+
+    /**
+     * @Route(methods={"POST"})
+     */
+    public function createAction(string $bookID)
+    {
+        return $this->wrapRequest(function () use ($bookID) {
+            $uuid = Uuid::uuid4();
+
+            $this->handle(
+                new RegisterBookCopy(
+                    $uuid->toString(),
+                    Uuid::fromString($bookID)->toString()
+                )
+            );
+
+            return $this->successResponse(['id' => $uuid], Response::HTTP_CREATED);
+        });
+    }
+
+    /**
+     * @Route("/{id}", methods={"PUT"})
+     */
+    public function updateAction(string $bookID, string $id)
+    {
+        return $this->wrapRequest(function () use ($id) {
+            $this->handle(
+                new UpdateBookCopyRemarks(
+                    Uuid::fromString($id)->toString(),
+                    $this->requestParam('remarks')
+                )
+            );
+
+            return $this->successResponse();
         });
     }
 
