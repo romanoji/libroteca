13b13b4d6447bbca57ad674ed27d919c00db9b09
Roman Józwiak <romanoji@users.noreply.github.com>
2017-08-18 01:39:23 +0200

    Enable facades and Eloquent for Lumen framework

--- src/UI/Web/Lumen/Application/bootstrap/app.php
+++ src/UI/Web/Lumen/Application/bootstrap/app.php
@@ -1,7 +1,5 @@
 <?php
 
-//require_once __DIR__.'/../vendor/autoload.php';
-
 try {
     (new Dotenv\Dotenv(__DIR__.'/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
@@ -23,9 +21,8 @@ $app = new Laravel\Lumen\Application(
     realpath(__DIR__.'/../')
 );
 
-// $app->withFacades();
-
-// $app->withEloquent();
+$app->withFacades();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
