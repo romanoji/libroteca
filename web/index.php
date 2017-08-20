4bf473201184a6a8301494fc4f2bdf3d603a4803
Roman Józwiak <romanoji@users.noreply.github.com>
2017-08-20 22:58:50 +0200

    Create docker-compose for each framework & app stack - Symfony with PostgreSQL & Lumen with MongoDB

--- web/index.php
+++ web/index.php
@@ -1,17 +1,18 @@
 <?php
 
-//use RJozwiak\Libroteca\UI\Web\Symfony;
+use RJozwiak\Libroteca\UI\Web\Symfony;
 use RJozwiak\Libroteca\UI\Web\Lumen;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-/* Choose your framework: */
-
-// Symfony //
-//$debugMode = true;
-//Symfony\Application::run($debugMode);
-
-// Lumen //
-Lumen\Application::run();
-
-// TODO: resolve which app to run by x-header or sth
+switch (getenv('FRAMEWORK')) {
+    case 'symfony':
+        $debugMode = true;
+        Symfony\Application::run($debugMode);
+        break;
+    case 'laravel':
+        Lumen\Application::run();
+        break;
+    default:
+        throw new RuntimeException('No framework specified.');
+}
